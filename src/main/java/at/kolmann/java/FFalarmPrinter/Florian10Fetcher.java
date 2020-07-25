51c617a3e63fa27c35baf4caf5790c3646d91b62
Philipp Kolmann <philipp@kolmann.at>
2020-07-25 08:21:23 +0200

    Force TLS1.2

--- src/main/java/at/kolmann/java/FFalarmPrinter/Florian10Fetcher.java
+++ src/main/java/at/kolmann/java/FFalarmPrinter/Florian10Fetcher.java
@@ -2,12 +2,15 @@ package at.kolmann.java.FFalarmPrinter;
 
 import org.json.JSONObject;
 
+import javax.net.ssl.SSLContext;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.net.http.HttpClient;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 public class Florian10Fetcher {
     private final HttpClient httpClient = HttpClient.newBuilder()
@@ -27,6 +30,15 @@ public class Florian10Fetcher {
     public JSONObject fetchFlorian10Data(String urlToFetch) {
         try {
             URL url = new URL(urlToFetch);
+
+
+            // Force to TLS1.2
+            // currently (2020-07-25) TLS1.3 fails with florian10
+            SSLContext context = SSLContext.getInstance("TLSv1.2");
+            context.init(null, null, null);
+            SSLContext.setDefault(context);
+
+
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
@@ -49,11 +61,17 @@ public class Florian10Fetcher {
             return florian10Data;
 
         } catch (MalformedURLException mue) {
-            System.out.println("Malformed URL exception....");
+            System.out.println("Florian10Fetcher: Malformed URL exception....");
             mue.printStackTrace();
         } catch (IOException ioe) {
-            System.out.println("IOException....");
+            System.out.println("Florian10Fetcher: IOException....");
             ioe.printStackTrace();
+        } catch (KeyManagementException e) {
+            System.out.println("Florian10Fetcher: KeyManagementException....");
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Florian10Fetcher: NoSuchAlgorithmException....");
+            e.printStackTrace();
         }
 
         return null;
