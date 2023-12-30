82daadd677e84eab288c9fa31f969113355fedb1
Philipp Kolmann <philipp.kolmann@fhwn.ac.at>
2023-12-30 14:39:10 +0100

    Try to fix missing cookie setting

--- src/main/java/at/kolmann/java/FFalarmPrinter/Florian10Fetcher.java
+++ src/main/java/at/kolmann/java/FFalarmPrinter/Florian10Fetcher.java
@@ -2,55 +2,49 @@ package at.kolmann.java.FFalarmPrinter;
 
 import org.json.JSONObject;
 
-import javax.net.ssl.SSLContext;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.net.http.HttpClient;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
 
 public class Florian10Fetcher {
-    private final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .build();
+    private final HttpClient httpClient;
 
+    private final CookieManager cookieManager;
     public Florian10Fetcher(String cookieFile) {
-        CookieManager cookieManager = new CookieManager(
+        this.cookieManager = new CookieManager(
                 new PersistentCookieStore(
                         System.getProperty("user.dir") + File.separator,
                         cookieFile
                 ),
-                null);
-        CookieHandler.setDefault(cookieManager);
+                CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+        CookieHandler.setDefault(this.cookieManager);
+
+        httpClient =  HttpClient
+                .newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .cookieHandler(this.cookieManager)
+                .build();
     }
 
     public JSONObject fetchFlorian10Data(String urlToFetch) {
         try {
-            URL url = new URL(urlToFetch);
+            HttpURLConnection connection = getHttpURLConnection(urlToFetch);
 
+            List<HttpCookie> cookies = this.cookieManager.getCookieStore().getCookies();
+            System.out.println("Cookies Cound: " + cookies.size());
+            for (HttpCookie cookie: cookies) {
+                System.out.println(cookie);
+            }
 
-            // Force to TLS1.2
-            // currently (2020-07-25) TLS1.3 fails with florian10
-            SSLContext context = SSLContext.getInstance("TLSv1.2");
-            context.init(null, null, null);
-            SSLContext.setDefault(context);
-
-
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-//            connection.setRequestProperty("Cache-Control", "no-cache");
-//            connection.setRequestProperty("Pragma", "no-cache");
-//            connection.setRequestProperty("Upgrade-Insecure-Requests", "1");
-//            connection.setRequestProperty("Accept-Encoding", "gzip, deflate,br");
-//            connection.setRequestProperty("Accept-Language", "de,en-US;q=0.7,en;q=0.3");
-//            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0");
-
-
-            connection.setRequestMethod("GET");
-
+            Map<String, List<String>> headers = connection.getHeaderFields();
+            for (String key : headers.keySet()) {
+                System.out.println("    * " + key + ":" + headers.get(key));
+            }
 //            System.out.println("Connection Response: " + connection.getResponseCode());
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 JSONObject error = new JSONObject();
@@ -76,14 +70,25 @@ public class Florian10Fetcher {
         } catch (IOException ioe) {
             System.out.println("Florian10Fetcher: IOException....");
             ioe.printStackTrace();
-        } catch (KeyManagementException e) {
-            System.out.println("Florian10Fetcher: KeyManagementException....");
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("Florian10Fetcher: NoSuchAlgorithmException....");
-            e.printStackTrace();
         }
 
         return null;
+    }
+
+    private static HttpURLConnection getHttpURLConnection(String urlToFetch) throws IOException {
+        URL url = new URL(urlToFetch);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+//            connection.setRequestProperty("Cache-Control", "no-cache");
+//            connection.setRequestProperty("Pragma", "no-cache");
+//            connection.setRequestProperty("Upgrade-Insecure-Requests", "1");
+//            connection.setRequestProperty("Accept-Encoding", "gzip, deflate,br");
+//            connection.setRequestProperty("Accept-Language", "de,en-US;q=0.7,en;q=0.3");
+//            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0");
+
+
+        connection.setRequestMethod("GET");
+        return connection;
     }
 }
