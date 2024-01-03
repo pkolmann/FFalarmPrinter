dd1292121a992f0e3da7bb3521978b74b249311e
Philipp Kolmann <philipp.kolmann@fhwn.ac.at>
2024-01-03 12:07:21 +0100

    Add Bootstrap to index.html and ArchivePageGenerator.java

--- src/main/java/at/kolmann/java/FFalarmPrinter/ArchivePageGenerator.java
+++ src/main/java/at/kolmann/java/FFalarmPrinter/ArchivePageGenerator.java
@@ -46,7 +46,10 @@ public class ArchivePageGenerator {
         archiv.append("<html lang=\"de\">\n");
         archiv.append("<head>\n");
         archiv.append("    <meta charset=\"UTF-8\">\n");
+        archiv.append("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
         archiv.append("    <title>FF Archiv</title>\n");
+        archiv.append("    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">\n");
+        archiv.append("    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>\n\n");
         archiv.append("    <link href=\"template.css\" rel=\"stylesheet\" type=\"text/css\" />\n");
         archiv.append("</head>\n");
         archiv.append("<body>\n");
